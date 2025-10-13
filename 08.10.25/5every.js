73acc37bc68ed1e95350bee107dff14556c313ef
Elen-Sahakyan <elensahakyann@gmail.com>
2025-10-14 01:35:39 +0400

    every

---
+++ 08.10.25/5every.js
@@ -0,0 +1,23 @@
+function every(arr, fn) {
+    const size = arr.length;
+
+    for(let i = 0; i < size; i++) {
+        if(!(isSmallerThan50(arr[i]))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const arr = [1, 4, 6, 8, 10, 15, 49, 51];
+
+function isSmallerThan50(value) {
+    return value < 50;
+}
+
+console.log(every(arr, isSmallerThan50));
+
+
+
+
+
