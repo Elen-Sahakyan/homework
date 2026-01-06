b1a2f57170d9929ae71cbe309c317fb25b8154af
Elen-Sahakyan <elensahakyann@gmail.com>
2026-01-06 19:34:38 +0400

    EventEmitter

---
+++ 03.12.25/task6.js
@@ -0,0 +1,22 @@
+const timestampable = {
+	this.createdAt: new Date(),
+	this.updatedAt: new Date(),
+	this.touch() {
+		this.createdAt = new Date();
+	}
+}
+
+class User {
+	constructor
+}
+
+
+
+
+
+
+
+
+
+
+
