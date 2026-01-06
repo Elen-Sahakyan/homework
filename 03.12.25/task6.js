0f4474557836e17c1b3ea85fa4e4369e728be90f
Elen-Sahakyan <elensahakyann@gmail.com>
2026-01-06 21:08:38 +0400

    timestamp

--- 03.12.25/task6.js
+++ 03.12.25/task6.js
@@ -1,19 +1,75 @@
-const timestampable = {
-	this.createdAt: new Date(),
-	this.updatedAt: new Date(),
-	this.touch() {
-		this.createdAt = new Date();
+const timestampable = { 
+	touch() {
+		this.updatedAt = new Date();
 	}
 }
 
 class User {
-	constructor
+	constructor(name, age) {
+		this.createdAt = new Date();
+		this.updatedAt = new Date();
+		this.name = name;
+		this.age = age;
+	}
 }
 
+Object.assign(User.prototype, timestampable);
 
+const u1 = new User("Elen", 22);
 
+console.log(u1.updatedAt);
 
+u1.name = "Alice";
+u1.touch();
 
+console.log(u1.updatedAt);
+
+class Post {
+	constructor(author, likes) {
+		this.createdAt = new Date();
+		this.updatedAt = new Date();
+		this.author = author;
+		this.likes = likes;
+	}
+}
+
+Object.assign(Post.prototype, timestampable);
+
+const p1 = new Post("elensahakyan_", 90);
+
+console.log(p1.updatedAt);
+
+p1.likes = 91;
+p1.touch();
+
+console.log(p1.updatedAt);
+
+class Comment {
+	constructor(id) {
+		this.createdAt = new Date();
+		this.updatedAt = new Date();
+		this._id = id;
+	}
+
+	set id(value) {
+		this._id = value;
+		this.touch();
+	}
+
+	get id() {
+		return this._id;
+	}
+}
+
+Object.assign(Comment.prototype, timestampable);
+
+const c1 = new Comment(1);
+
+console.log(c1.updatedAt);
+
+c1.id = 2;
+
+console.log(c1.updatedAt);
 
 
 
